4f192c44a766f9948ff31102a7a068e24c9c47e8
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-11-17 18:37:07 -0600

    added

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,10 @@
 import 'bootstrap';
 import '../styles/main.scss';
+import firebase from 'firebase';
+import apiKeys from './helpers/apiKeys.json';
+
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+};
+
+init();
