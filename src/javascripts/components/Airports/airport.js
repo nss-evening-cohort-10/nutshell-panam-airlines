e84a516342d97f3a7a5b2fe6050b90bd2a3a7d3c
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-11-17 20:48:03 -0600

    cards are printing

--- src/javascripts/components/Airports/airport.js
+++ src/javascripts/components/Airports/airport.js
@@ -1,28 +1,29 @@
+import $ from 'jquery';
 import './airport.scss';
 import airportsData from '../../helpers/data/airportsData';
 import utilities from '../../helpers/utilities';
 
+const displayAirports = () => {
+  $('#airports-link').on('click', () => {
+    $('#airports').show();
+  });
+};
+
 const createAirportCard = () => {
   airportsData.getAllAirports()
     .then((airports) => {
-      let domString = '<h1 class="airports-title">Airports</h1>';
-      domString += '<div id="airports" class="d-flex flex-wrap">';
+      let domString = '<h1 class="airports-title text-center">Airports</h1>';
+      domString += '<div id="airports-section" class="d-flex flex-wrap text-center offset-2">';
       airports.forEach((airport) => {
         domString += `
-        <div class="card col-4" id="${airport.id}">
-          <div class="card-header">
-            <h2>${airport.name}</h2>
-          </div>
+        <div id="${airport.id}" class="card crew-card" style="width: 18rem;">
+          <img src="${airport.imageUrl}" class="card-img-top crew-image" alt="${airport.name}">
           <div class="card-body">
-            <div class="d-flex flex-wrap justify-content-between">
-              <img class="card-title">${airport.image}</img>
-              <p class="card-text">${airport.location}</p>
-            </div>
-            <button class="btn btn-light" id="add-airport">Add</button>
-            <button class="btn btn-light" id="edit-airport">Edit</button>
-            <button class="btn btn-light" id="delete-airport">Delete</button>
+            <h5 class="card-title">${airport.name}</h5>
+            <p class="card-text">${airport.location}</p>
           </div>
-        </div>`;
+        </div>
+        `;
       });
       domString += '</div>';
       utilities.printToDom('airports', domString);
@@ -30,4 +31,4 @@ const createAirportCard = () => {
     .catch((error) => console.error(error));
 };
 
-export default { createAirportCard };
+export default { createAirportCard, displayAirports };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,9 +2,14 @@ import 'bootstrap';
 import '../styles/main.scss';
 import firebase from 'firebase';
 import apiKeys from './helpers/apiKeys.json';
+import airportsData from './helpers/data/airportsData';
+import airport from './components/Airports/airport';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
+  airportsData.getAllAirports();
+  airport.createAirportCard();
+  airport.displayAirports();
 };
 
 init();
