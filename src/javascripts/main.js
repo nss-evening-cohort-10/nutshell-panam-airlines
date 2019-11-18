f560cb299fea68fcbe5795de66f55b6900096d12
Connor Sullivan <cksullvn@gmail.com>
2019-11-18 12:04:16 -0600

    auth works

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
 import apiKeys from './helpers/apiKeys.json';
-// import Navbar from './components/Navbar/navbar';
+import Navbar from './components/Navbar/navbar';
 import authData from './helpers/data/authData';
 import airport from './components/Airports/airport';
 
@@ -12,7 +12,7 @@ import crew from './components/crew/crew';
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   authData.checkLoginStatus();
-  // Navbar.attachEvents();
+  Navbar.attachEvents();
   airport.createAirportCard();
   airport.displayAirports();
   crew.createCrewCard();
