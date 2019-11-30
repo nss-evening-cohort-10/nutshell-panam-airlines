a30c8591b202cca912898b7f72b1a081fe3e5573
Emilee Mitchell <emcakes@gmail.com>
2019-11-30 13:05:55 -0600

    Added Flights Data file

--- src/javascripts/helpers/data/flightsData.js
+++ src/javascripts/helpers/data/flightsData.js
@@ -1,1 +1,19 @@
-import axios from 'axios'
+import axios from 'axios';
+import apiKeys from  '../apiKeys.json';
+
+const baseURL = apiKeys.firebaseKeys.databaseURL;
+
+const getAllFlights = () => new Promise((resolve,reject) => {
+  axios.get(`${baseUrl}/shows.json`)
+  .then((response) => {
+    const flights = response.data;
+    const flightsWithIds = [];
+    Object.keys(flights).forEach((fbId) => {
+      flights[fbId].id = fbId;
+    });
+    resolve(flightsWithIds);
+  })
+  ,catch((err) => reject(err));
+});
+
+export default { getAllFlights };
